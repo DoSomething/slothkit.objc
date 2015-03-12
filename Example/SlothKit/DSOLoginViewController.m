af24925e5abb3b1060458077bfa72402a886a52b
Aaron Schachter <aaronschachter@gmail.com>
2015-03-12 11:16:46 -0700

    Removes unused boilerplate methods

--- Example/SlothKit/DSOLoginViewController.m
+++ Example/SlothKit/DSOLoginViewController.m
@@ -18,26 +18,6 @@
 
 @implementation DSOLoginViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)submitTapped:(id)sender {
     NSString *username = self.usernameTextField.text;
     NSString *password = self.passwordTextField.text;
